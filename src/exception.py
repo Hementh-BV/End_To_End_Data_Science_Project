ae8bd7006637a89f3fbac7da9fb19b1cba77ae52
Hemanth <hemanth.guitar17@gmail.com>
2024-02-24 21:54:24 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+# import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -28,4 +29,3 @@ class CustomException(Exception):
 #     except Exception as e:
 #         logging.info("Divide by Zero Error")
 #         raise CustomException(e, sys)
-    
